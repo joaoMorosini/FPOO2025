bbba23f6c0fad58cd339f808b47972256319d0e2
joaoMorosini <stachakmorosinijoaoantonio@gmail.com>
2025-04-02 14:57:50 -0300

    Primeiros passos

---
+++ Projeto Semaforo/semaphore/src/main/java/com/utfpr_fpoo/semaphore_light/light.java
@@ -0,0 +1,12 @@
+package com.utfpr_fpoo.semaphore_light;
+
+public interface light {
+    enum OnOff {ON, OFF};
+
+    void turnOn();
+    void turnOff();
+
+    boolean isOn();
+    boolean isOff();
+    
+} 
