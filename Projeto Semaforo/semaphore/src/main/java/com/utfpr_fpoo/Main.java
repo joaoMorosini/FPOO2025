02297fe02fba4bfb2ca524aa8e47eef53a642ef8
joaoMorosini <stachakmorosinijoaoantonio@gmail.com>
2025-04-04 22:04:18 -0300

    Primeiros codigos - Aula dia 04/04/2025

--- Projeto Semaforo/semaphore/src/main/java/com/utfpr_fpoo/Main.java
+++ Projeto Semaforo/semaphore/src/main/java/com/utfpr_fpoo/Main.java
@@ -1,7 +1,12 @@
 package com.utfpr_fpoo;
 
+import com.utfpr_fpoo.semaphore_light.SimpleLight;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SimpleLight simpleLight = new SimpleLight();
+        simpleLight.turnOff();
+        System.out.println("On: " + simpleLight.isOff());
+        System.out.println("Off: " + simpleLight.isOn());
     }
 }
---
+++ Projeto Semaforo/semaphore/src/main/java/com/utfpr_fpoo/semaphore_light/SimpleLight.java
@@ -0,0 +1,24 @@
+package com.utfpr_fpoo.semaphore_light;
+
+public class SimpleLight implements Light{
+    
+    private OnOff state = OnOff.OFF;
+  
+    @Override
+    public void turnOn(){
+        this.state = OnOff.ON;
+    } 
+    @Override
+    public void turnOff(){
+        this.state = OnOff.OFF;
+    } 
+    
+    @Override
+    public boolean isOn(){
+        return this.state == OnOff.ON; 
+    } 
+    @Override
+    public boolean isOff(){
+        return this.state == OnOff.OFF;
+    } 
+}
