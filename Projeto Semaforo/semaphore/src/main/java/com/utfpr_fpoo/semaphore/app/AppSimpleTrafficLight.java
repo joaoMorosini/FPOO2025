da0d629eef7c8c38abcdd8b59281b97e7e0b0674
joaoMorosini <stachakmorosinijoaoantonio@gmail.com>
2025-05-13 21:53:05 -0300

    Aula dia 13/05/2025

--- Projeto Semaforo/semaphore/src/main/java/com/utfpr_fpoo/semaphore/app/AppSimpleTrafficLight.java
+++ Projeto Semaforo/semaphore/src/main/java/com/utfpr_fpoo/semaphore/app/AppSimpleTrafficLight.java
@@ -3,7 +3,10 @@ package com.utfpr_fpoo.semaphore.app;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import com.utfpr_fpoo.semaphore.trafficLight.control.SimpleTrafficLightControl;
 import com.utfpr_fpoo.semaphore.trafficLight.simple.SimpleTrafficLight;
 import com.utfpr_fpoo.semaphore.util.gui.MyWindow;
 
@@ -11,13 +14,38 @@ public class AppSimpleTrafficLight {
     public static void main(String[] args) throws IOException {
 
     SimpleTrafficLight defaultOne = new SimpleTrafficLight();
+    SimpleTrafficLightControl defaultControl = new SimpleTrafficLightControl(defaultOne);
 
     Point position = new Point(100, 100);
     Dimension dimension = new Dimension(100, 350);
     SimpleTrafficLight customOne = new SimpleTrafficLight(position, dimension);
-    
+    SimpleTrafficLightControl customControl = new SimpleTrafficLightControl(customOne);
+
     MyWindow window = new MyWindow();
     window.add(defaultOne);
     window.add(customOne);
-    }
+
+    customControl.turnAlert();
+
+    Timer timer = new Timer();
+    timer.scheduleAtFixedRate(new TimerTask() {
+
+            private int i =0;
+
+            @Override
+                public void run(){
+                    i++;
+                    if((i % 3) == 1)
+                        defaultControl.turnGreen();
+                    if((i % 3 == 2))
+                        defaultControl.turnYellow();
+                    if((i % 3 == 0)){
+                        defaultControl.turnRed();
+                        i = 0;
+                    }
+                }
+    },
+    0,2_000);
+}
+
 }
